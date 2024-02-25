5c946b9bae276f69f8a715a3cef341142b06ea5b
prafuljain123 <prafuljain303@gmail.com>
2024-02-25 23:36:29 +0530

    update coreOptions

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ const cookiParser = require('cookie-parser');
 
 
 const port = process.env.PORT || 3000;
-
 const IP = require('ip');
+
+const corsOptions = {
+    origin: 'https://login-system-u503.onrender.com/', // Replace with the origin of your frontend application
+    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(router);
+app.use(cookiParser());
+// app.use(cors());
 
 app.get('/', (req, res) => {
     const ipAddress = IP.address();
@@ -16,12 +29,29 @@ app.get('/', (req, res) => {
 });
 
 
-app.use(express.json());
-app.use(router);
-app.use(cookiParser());
-app.use(cors());
 
 
 app.listen( port , ()=>{
     console.log(`Server is Running at PORT ${port} and IP: ${IP.address()}`);   
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
